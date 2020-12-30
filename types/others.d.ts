aac9d5a89b93a5f3a62f9d556ddcfe568e323118
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-12-30 12:59:37 +0100

    Removed unused types

--- types/others.d.ts
+++ types/others.d.ts
@@ -1,17 +1,6 @@
 type Block = import("wordpress__blocks").BlockInstance;
 
-type BlockType = import("wordpress__blocks").Block;
-
 type BlockId = Block["clientId"];
-
-interface ComponentProps {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	[prop: string]: any;
-
-	id?: string | null;
-	className?: string | null | (string | null)[] | undefined;
-	children?: import("react").ReactNode;
-}
 
 interface DropArea {
 	id: BlockId;

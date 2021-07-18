464ba0c932973b97538df627b8014ccf7d405c8d
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-07-18 16:52:15 +0200

    Added initial block styles

--- src/components/block/block.tsx
+++ src/components/block/block.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { useContext } from "@wordpress/element";
 
+import styles from "./styles.styl";
 import type { Component } from "./types";
 import { context } from "./context";
+import { useClassName } from "@/utils";
 
 export const Block: Component = props => {
 	const { style } = props;
 
-	const { id } = useContext(context);
+	const { id, ancestors_id } = useContext(context);
 
-	return <div style={style}>{id}</div>;
+	const className_container = useClassName(
+		styles.container,
+		styles[`level-${ancestors_id.length}`]
+	);
+
+	return (
+		<div style={style} className={className_container}>
+			<div className={styles.content}>{id}</div>
+		</div>
+	);
 };

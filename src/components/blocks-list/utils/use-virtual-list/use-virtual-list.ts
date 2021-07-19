232f4886145ee3e0ae708008534bda1d0f0290b0
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-07-19 19:01:34 +0200

    Fixed useVirtualList initial trigger

--- src/components/blocks-list/utils/use-virtual-list/use-virtual-list.ts
+++ src/components/blocks-list/utils/use-virtual-list/use-virtual-list.ts
@@ -1,4 +1,10 @@
-import { useMemo, useRef, useState, useEffect } from "@wordpress/element";
+import {
+	useMemo,
+	useRef,
+	useState,
+	useEffect,
+	useCallback,
+} from "@wordpress/element";
 
 import styles from "./styles.styl";
 import { useWindowSize } from "@/utils";
@@ -19,49 +25,50 @@ export const useVirtualList: Util = props => {
 
 	const { window_width, window_height } = useWindowSize();
 
-	useEffect(() => {
-		setContainerHeight($container.current?.clientHeight || 0);
-	}, [window_width, window_height]);
-
-	useEffect(() => {
+	const updateItemsStyle = useCallback(() => {
 		if (!$container.current) return;
 
 		const offset = container_height / 2;
 		const items_length_array = Array(items_length).fill(null);
+		const scroll_top = $container.current.scrollTop || 0;
+		const boundary_top = scroll_top - item_height - offset;
+		const boundary_bottom = scroll_top + container_height + offset;
 
-		const updateItemsStyle = () => {
-			const scroll_top = $container.current?.scrollTop || 0;
-			const boundary_top = scroll_top - item_height - offset;
-			const boundary_bottom = scroll_top + container_height + offset;
+		const items_style = items_length_array.map((_, index) => {
+			const top = index * item_height;
 
-			const items_style = items_length_array.map((_, index) => {
-				const top = index * item_height;
+			if (items_index_to_keep_rendered?.includes(index)) {
+				return { top };
+			}
 
-				if (items_index_to_keep_rendered?.includes(index)) {
-					return { top };
-				}
+			if (boundary_top <= top && boundary_bottom > top) {
+				return { top };
+			}
 
-				if (boundary_top <= top && boundary_bottom > top) {
-					return { top };
-				}
+			return undefined;
+		});
 
-				return undefined;
-			});
-
-			setItemsStyle(items_style);
-		};
-
-		$container.current?.addEventListener("scroll", updateItemsStyle);
-
-		return () => {
-			$container.current?.removeEventListener("scroll", updateItemsStyle);
-		};
+		setItemsStyle(items_style);
 	}, [
 		container_height,
 		items_length,
 		item_height,
 		items_index_to_keep_rendered,
 	]);
+
+	useEffect(() => {
+		setContainerHeight($container.current?.clientHeight || 0);
+	}, [window_width, window_height]);
+
+	useEffect(() => {
+		updateItemsStyle();
+
+		$container.current?.addEventListener("scroll", updateItemsStyle);
+
+		return () => {
+			$container.current?.removeEventListener("scroll", updateItemsStyle);
+		};
+	}, [updateItemsStyle]);
 
 	return {
 		$container,

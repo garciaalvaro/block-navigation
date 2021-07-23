28bd8ea7558e6bd5ce06b4138c48b5c9608b3aa6
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-07-23 13:24:58 +0200

    Initial refactor of block

--- src/components/block/block.tsx
+++ src/components/block/block.tsx
@@ -1,24 +1,55 @@
 import React from "react";
+import { useSelect } from "@wordpress/data";
 import { useContext } from "@wordpress/element";
 
 import styles from "./styles.styl";
 import type { Component } from "./types";
+import { useMovingAttributes, useMovingClasses, useSelectBlock } from "./utils";
 import { context } from "./context";
 import { useClassName } from "@/utils";
+import { BlockDropAreas } from "../block-drop-areas";
+import { store_slug } from "@/store";
 
 export const Block: Component = props => {
 	const { style } = props;
 
 	const { id, ancestors_id } = useContext(context);
 
+	const moving_block = useSelect(select => select(store_slug).moving_block());
+
+	const { className: className_select, ...select_attributes } =
+		useSelectBlock();
+
+	const { can_move, moving_is_over, is_moving, ...moving_attributes } =
+		useMovingAttributes();
+
+	const {
+		className_container: className_container_moving,
+		className_content: className_content_moving,
+	} = useMovingClasses({ can_move, moving_is_over, is_moving });
+
 	const className_container = useClassName(
 		styles.container,
-		styles[`level-${ancestors_id.length}`]
+		styles[`level-${ancestors_id.length}`],
+		className_container_moving
 	);
 
+	const className_content = useClassName({
+		[styles.content]: true,
+		[className_content_moving]: true,
+		[className_select]: !moving_block,
+	});
+
 	return (
-		<div style={style} className={className_container}>
-			<div className={styles.content}>{id}</div>
+		<div
+			style={style}
+			className={className_container}
+			{...moving_attributes}
+			{...select_attributes}
+		>
+			<BlockDropAreas />
+
+			<div className={className_content}>{id}</div>
 		</div>
 	);
 };

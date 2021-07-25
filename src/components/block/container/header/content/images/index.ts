4063a054d76c4c66129a6eba3b786a17dc8742de
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-07-25 18:37:13 +0200

    Removed no longer used files

--- src/components/block/container/header/content/images/index.ts
+++
@@ -1,1 +0,0 @@
-export { Images } from "./images";
--- src/components/block/container/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export { useDrop } from "./use-drop";
-export { useIsSelected } from "./use-is-selected";
-export { useMovingIsOver } from "./use-moving-is-over";

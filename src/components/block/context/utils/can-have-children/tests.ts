304076e8b47e56a8da30bd2504de65bd77101c4f
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-07-25 18:39:44 +0200

    Fixed tests not including mock

--- src/components/block/context/utils/can-have-children/tests.ts
+++ src/components/block/context/utils/can-have-children/tests.ts
@@ -4,7 +4,7 @@ import { mockSelect } from "@/utils-test";
 jest.mock("@wordpress/data");
 
 test("canHaveChildren returns correctly when moving_block has value", () => {
-	mockSelect({ canInsertBlockType: () => true });
+	mockSelect({ canInsertBlockType: () => true, is_expanded: () => true });
 
 	const moving_block = {
 		id: "",
@@ -14,19 +14,19 @@ test("canHaveChildren returns correctly when moving_block has value", () => {
 
 	expect(canHaveChildren("", moving_block)).toBe(true);
 
-	mockSelect({ canInsertBlockType: () => false });
+	mockSelect({ canInsertBlockType: () => false, is_expanded: () => true });
 
 	expect(canHaveChildren("", moving_block)).toBe(false);
 });
 
 test("canHaveChildren returns correctly when moving_block is null", () => {
-	mockSelect({ canInsertBlockType: () => true });
+	mockSelect({ canInsertBlockType: () => true, is_expanded: () => true });
 
 	const moving_block = null;
 
 	expect(canHaveChildren("", moving_block)).toBe(false);
 
-	mockSelect({ canInsertBlockType: () => false });
+	mockSelect({ canInsertBlockType: () => false, is_expanded: () => true });
 
 	expect(canHaveChildren("", moving_block)).toBe(false);
 });

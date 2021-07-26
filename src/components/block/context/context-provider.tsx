e2085fafc7ab4ef9eb2fb83da5c3d014b50a8e73
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-07-26 22:05:26 +0200

    Use getAncestorsId util in block context

--- src/components/block/context/context-provider.tsx
+++ src/components/block/context/context-provider.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 import { useMemo } from "@wordpress/element";
-import { useSelect } from "@wordpress/data";
 
 import type { Component } from "./types";
 import { context } from "./context";
-import type { BlockId } from "@/types";
 import { useDropAreas } from "./utils";
+import { getAncestorsId } from "@/utils";
 
 export const ContextProvider: Component = props => {
 	const { id } = props;
 
-	const ancestors_id: BlockId[] = useSelect(select =>
-		select(
-			"core/block-editor"
-			// @ts-expect-error @wordpress/block-editor types are outdated
-		).getBlockParents(id)
-	);
+	const ancestors_id = useMemo(() => getAncestorsId(id), [id]);
 
 	const parent_id = useMemo(
 		() => ancestors_id.slice(-1)[0] || "",

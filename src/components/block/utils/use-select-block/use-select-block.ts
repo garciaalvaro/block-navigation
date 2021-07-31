783092a76350976734f78aa0dda0d580f8e0b448
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-07-31 23:00:35 +0200

    Prevent selecting block when moving by click

--- src/components/block/utils/use-select-block/use-select-block.ts
+++ src/components/block/utils/use-select-block/use-select-block.ts
@@ -2,6 +2,7 @@ import { useSelect, useDispatch } from "@wordpress/data";
 import { useState, useLayoutEffect, useContext } from "@wordpress/element";
 
 import { useClassName } from "@/utils";
+import { store_slug } from "@/store";
 
 import styles from "./styles.styl";
 import type { Util } from "./types";
@@ -9,6 +10,8 @@ import { context } from "../../context";
 
 export const useSelectBlock: Util = () => {
 	const { id } = useContext(context);
+
+	const moving_block = useSelect(select => select(store_slug).moving_block());
 
 	const is_selected_multiple = useSelect(select =>
 		select("core/block-editor").getSelectedBlockClientIds().includes(id)
@@ -25,6 +28,8 @@ export const useSelectBlock: Util = () => {
 	const { selectBlock } = useDispatch("core/block-editor");
 
 	const onClick = () => {
+		if (moving_block) return;
+
 		setIsSelected(true);
 		selectBlock(id);
 	};

211b79ca5b138d4bac19ebd5f9fac89583ab4177
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-08-18 21:35:50 +0200

    Fixed block-drop-areas incorrect height in detached

--- src/components/block-drop-areas/block-drop-areas.tsx
+++ src/components/block-drop-areas/block-drop-areas.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import type { FunctionComponent } from "react";
 import { useContext } from "@wordpress/element";
+import { useSelect } from "@wordpress/data";
+
+import { store_slug } from "@/store";
+import { useClassName } from "@/utils";
 
 import styles from "./styles.styl";
 import { DropArea } from "./components";
@@ -9,13 +13,20 @@ import { context } from "../block";
 export const BlockDropAreas: FunctionComponent = () => {
 	const { drop_areas } = useContext(context);
 
+	const is_detached = useSelect(select => select(store_slug).is_detached());
+
+	const className = useClassName([is_detached], {
+		[styles.container]: true,
+		[styles.is_detached]: is_detached,
+	});
+
 	if (!drop_areas.length) {
 		return null;
 	}
 
 	return (
 		<div
-			className={styles.container}
+			className={className}
 			onDragLeave={e => {
 				// https://stackoverflow.com/a/54271161 | CC BY-SA 3.0
 				// Prevent drag leave from triggering when switching
